f859e9ebad8606c9794077be95b2be88d1c629ba
mastamysta <benjamin-read@hotmail.co.uk>
2020-05-16 11:59:16 +0100

    Added delete all functionality and crypto authentication

--- index.js
+++ index.js
@@ -45,10 +45,9 @@ const REGISTERHANDLER = (req, res) => {
 };
 
 const REGISTERHANDLERCRYPTO = (req, res) => {
-  const salt = crypto.randomBytes(16);
-  const hmac = crypto.createHmac("sha256", salt);
-  const hash = hmac.update(req.params.password).digest('base64');
-  const salthash = `${salt}M${hash}`;
+  const salt = crypto.randomBytes(16).toString("base64");
+  const hash = crypto.createHmac("sha256", salt).update(req.params.password).digest('base64');
+  const salthash = `${salt}$${hash}`;
   var thisUser = new user({userName: req.params.userName, password: salthash, email: req.params.email});
   thisUser.save(ENTRYERR);
   console.log(`Successfully entered:\n${req.params.userName}\n${req.params.password}\n${salthash}\n${req.params.email}\n\n\n`);
@@ -65,14 +64,14 @@ const REGISTERHANDLERCRYPTO = (req, res) => {
 const LOGINHANDLER = (req, res) => {
   var userForName = getUser(req.params.userName);
   userForName.then(function(result) {
-    var oldSalt = result.password.substring(0, 15);
-    console.log(`\n${oldSalt}\n`);
-    const hmac = crypto.createHmac("sha256", oldSalt);
-    const hash = hmac.update(req.params.password).digest('base64');
-    if(hash = userForName.password){
-      const newSalt = crypto.randomBytes(16);
-      const newHmac = crypto.createHmac("sha256", newSalt)
-      const newHash = newHmac.update(req.params.password).digest('base64');
+    var parts = result.password.split("$");
+    const newHash = crypto.createHmac("sha256", parts[0]).update(req.params.password).digest('base64');
+    console.log(`\n\nEntered Password:${req.params.password}\n\n`);
+    console.log(`\n\nDatabase Password Hash:${parts[1]}\n\n`);
+    console.log(`\n\nEntered Password Hash:${newHash}\n\n`);
+    if((`${parts[0]}$${newHash}`) == `${result.password}`){
+      const newSalt = crypto.randomBytes(16).toString("base64");
+      const newHash = crypto.createHmac("sha256", newSalt).update(req.params.password).digest('base64');
       const newSaltHash = `${newSalt}M${newHash}`;
       user.updateOne({userName: req.params.userName},{password: newSaltHash}, function(err, res) {
         //handler
@@ -93,8 +92,14 @@ const LOGINHANDLER = (req, res) => {
   });
 };
 
+function DELETEALLHANDLER(req, res){
+  deleteAllUsers();
+  res.send("All users deleted");
+}
+
 app.get("/register/userName/:userName/password/:password/email/:email", REGISTERHANDLERCRYPTO);
 app.get("/login/userName/:userName/password/:password", LOGINHANDLER);
+app.get("/deleteAll", DELETEALLHANDLER);
 
 async function getUsers(){
   return await user.find({}, null);
